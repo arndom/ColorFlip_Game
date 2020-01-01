0a4e1055bd902a05c75a7c8b9fe967e412e89287
JamesHole <JamesHole@koji-projects.com>
2020-01-01 10:50:15 +0000

    add new levels

--- frontend/components/Game/Game.js
+++ frontend/components/Game/Game.js
@@ -276,7 +276,7 @@ export class Game extends React.Component {
 						<StyledButton>
 							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
 						</StyledButton>
-						{(win &&
+						{(/*win &&*/
 							<StyledButton onClick={this.nextLevel}>	
 								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"/></svg>
 							</StyledButton>
